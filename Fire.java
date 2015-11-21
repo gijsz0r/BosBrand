0239eb29d7aa3fdf7d64a870a2fafe4c69080a67
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2015-11-21 17:26:11 +0100

    [Anton] Added some more stuff to Fire and Tree

--- Fire.java
+++ Fire.java
@@ -1,6 +1,11 @@
 package BosBrand;
 
+import java.util.List;
 import java.util.Random;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import repast.simphony.engine.schedule.ScheduleParameters;
 import repast.simphony.engine.schedule.ScheduledMethod;
@@ -19,13 +24,14 @@ public class Fire {
 
 	}
 
+	public GridPoint getLocation() {
+		return grid.getLocation(this);
+	}
+
 	@ScheduledMethod(start = 1, interval = 1, priority = ScheduleParameters.RANDOM_PRIORITY)
 	public void step() {
 		// Check if any trees spontaneously combust
 		// TODO: implement
-
-		// Chance of Fire moving = Base Chance + Rain Modifier + Wind Modifier
-		Random r = new Random();
 
 		// Get the grid location of this Fire
 		GridPoint pt = grid.getLocation(this);
@@ -35,16 +41,25 @@ public class Fire {
 			// Loop through all objects in the neighbouring cells
 			Iterable<Object> cellObjects = Direction
 					.getObjectsInAdjacentDirection(direction, grid, pt);
-			// Check if the tree has any health left, otherwise spreading there
-			// can't happen
-			// Check if there is fire already on the cell
-			// Calculate chance of spreading
-			// TODO: implement
+			// Convert iterable to a list
+			List<Object> cellObjectsList = StreamSupport.stream(
+					Spliterators.spliteratorUnknownSize(cellObjects.iterator(),
+							Spliterator.ORDERED), false).collect(
+					Collectors.toList());
+			// Get the Tree object on the cell (should always be 1)
+			if (cellObjectsList.stream().anyMatch(i -> i instanceof Tree)) {
+				Tree neighbourTree = (Tree) cellObjectsList.stream()
+						.filter(i -> i instanceof Tree).findFirst().get();
+				// Check if the tree has any health left and is not already
+				// burning
+				if (neighbourTree.getCurrentHP() > 0
+						&& !neighbourTree.getIsBurning()) {
+					// Chance of Fire moving = Base Chance + Rain Modifier +
+					// Wind Modifier
+					Random r = new Random();
+					// TODO: implement
+				}
+			}
 		}
 	}
-
-	public GridPoint getLocation() {
-		return grid.getLocation(this);
-	}
-
 }
